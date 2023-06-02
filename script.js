5956c875223b01e0defae98e52df2171cd40e649
Keyur Chauhan <130525335+keyur146@users.noreply.github.com>
2023-06-02 10:24:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock(){
+    const curruntDate = new Date()
+    const secondsRatio = curruntDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + curruntDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + curruntDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
